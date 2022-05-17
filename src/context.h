b4b9d85a788a30ef1dd33739d351a9af738dc324
Zach Wolfe <zachrwolfe@me.com>
2022-05-16 20:10:48 -0500

    Change the namespacing scheme of ctx variables
    This eliminates the need to copy from a static globally-scoped variable
    to a thread-local namespaced one. It also should mean that people will
    be able to define ctx vars inside of whatever namespace they want, with
    the same rules as regular variables. Including naming the full
    namespace in get/set_ctx, e.g., get_ctx(my::namespaced::ctx_var)

--- src/context.h
+++ src/context.h
@@ -4,21 +4,14 @@
 #include "macros.h"
 #include "defer.h"
 
-#define ZW_DECLARE_CTX_VAR(type, name) namespace zw::impl::ctx { \
-    extern thread_local type name; \
-};
-#define ZW_DEFINE_CTX_VAR(type, name, expression) \
-    type ZW_CONCAT(initializer_for_ ## name ## _on_line, __LINE__) = (expression);\
-    \
-    namespace zw::impl::ctx { \
-        thread_local type name = ZW_CONCAT(initializer_for_ ## name ## _on_line, __LINE__); \
-    };
+#define ZW_DECLARE_CTX_VAR(type, name) extern thread_local type name##_zw_ctx_var;
+#define ZW_DEFINE_CTX_VAR(type, name, expression) thread_local type name##_zw_ctx_var = (expression);
 
 #define zw_set_ctx(name, new_value, ...) \
-    auto ZW_CONCAT(__ctx_old_value__, __LINE__ ## __VA_ARGS__) = ::zw::impl::ctx::name; \
-    ::zw::impl::ctx::name = new_value; \
-    zw_defer(::zw::impl::ctx::name = ZW_CONCAT(__ctx_old_value__, __LINE__ ## __VA_ARGS__), __VA_ARGS__)
-#define zw_get_ctx(name) ([&] { return ::zw::impl::ctx::name; }())
+    auto ZW_CONCAT(__ctx_old_value__, __LINE__ ## __VA_ARGS__) = name##_zw_ctx_var; \
+    name##_zw_ctx_var = new_value; \
+    zw_defer(name##_zw_ctx_var = ZW_CONCAT(__ctx_old_value__, __LINE__ ## __VA_ARGS__), __VA_ARGS__)
+#define zw_get_ctx(name) ([&] { return name##_zw_ctx_var; }())
 #define use_temp_allocator() zw_set_ctx(allocator, zw_get_ctx(temp_allocator))
 #define using_temp_allocator(code) \
     auto ZW_CONCAT(__allocator_old_value__, __LINE__) = zw_get_ctx(allocator); \
