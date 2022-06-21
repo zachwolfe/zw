23f64ed31ca41f178d503f47773ba43b2143d50e
Zach Wolfe <zachrwolfe@me.com>
2022-06-21 02:25:18 -0500

    Make ranges iterable

--- src/range.h
+++ src/range.h
@@ -5,6 +5,9 @@ namespace zw {
 struct Range {
     size_t lower_bound = 0;
     size_t upper_bound = 0;
+
+    size_t begin() const { return lower_bound; }
+    size_t end() const { return upper_bound; }
 };
 
 };
